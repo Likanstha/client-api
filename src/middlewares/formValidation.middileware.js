03a871f56acf995435441d239e7f79c59e45c0cf
likanstha <likan.stha@gmail.com>
2022-07-10 20:27:07 +1000

    server ticket validation done

--- src/middlewares/formValidation.middileware.js
+++ src/middlewares/formValidation.middileware.js
@@ -6,7 +6,13 @@ const email = Joi.string().email({
 });
 
 const pin = Joi.number().min(10000).max(999999).required();
+//const phone = Joi.number().min(400000001).max(500000001).required();
+
 const newPassword = Joi.string().min(3).max(30).required();
+
+const shortStr = Joi.string().min(2).max(50);
+const longStr = Joi.string().min(2).max(1000);
+const dt = Joi.date();
 
 
 const resetPassReqValidation = (req, res, next) => {
@@ -28,7 +34,42 @@ const updatePassValidation = (req, res, next) => {
 	next();
 };
 
+const createNewTicketValidation = (req, res, next) => {
+	const schema = Joi.object({
+		subject: shortStr.required(),
+		sender: shortStr.required(),
+		message: longStr.required()
+	});
+
+	console.log(req.body);
+	const value = schema.validate(req.body);
+
+	if (value.error) {
+		return res.json({ status: "error", message: value.error.message });
+	}
+
+	next();
+};
+
+const replyTicketMessageValidation = (req, res, next) => {
+	const schema = Joi.object({
+		sender: shortStr.required(),
+		message: longStr.required(),
+	});
+
+	//console.log(req.body);
+	const value = schema.validate(req.body);
+
+	if (value.error) {
+		return res.json({ status: "error", message: value.error.message });
+	}
+
+	next();
+};
+
 module.exports={
     resetPassReqValidation,
     updatePassValidation,
+	createNewTicketValidation,
+	replyTicketMessageValidation,
 }
